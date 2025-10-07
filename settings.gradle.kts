e7d8ba30dff5d33050d6242de6975da1b25c5894
Kriti Singh Behl <kbehl1_be22@thapar.edu>
2025-10-07 17:06:31 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories { gradlePluginPortal(); google(); mavenCentral() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+
+// ADD THESE:
+rootProject.name = "Frontend_HG"
+include(":app")
